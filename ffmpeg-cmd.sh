494b11c2c4112f3a103037ed275ef6cfe6292dd0
anmg <a.gangan@gmail.com>
2022-02-21 12:12:53 +0200

    Update ffmpeg-cmd.sh

--- ffmpeg-cmd.sh
+++ ffmpeg-cmd.sh
@@ -10,7 +10,7 @@ ffmpeg -y -nostats -s 1280x800 \
 #	-probesize 16M \
 	-f x11grab -thread_queue_size 1024 \
 	-i :$DISPLAY_NUMBER \
-	# -f alsa \
+	-f alsa \
 	#-thread_queue_size 1024 \
 	-itsoffset 0.57 \
 	-i pulse -ac 2 \
