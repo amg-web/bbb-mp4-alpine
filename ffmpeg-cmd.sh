3337a9e8c13f432142dffff0315daed9832db571
anmg <a.gangan@gmail.com>
2022-02-21 12:38:30 +0200

    Update ffmpeg-cmd.sh

--- ffmpeg-cmd.sh
+++ ffmpeg-cmd.sh
@@ -1,21 +1,19 @@
 #!usr/bin/sh
 
-rc-service alsa start
-
 DURATION=$1
 EXPORT_NAME=$2
 DISPLAY_NUMBER=$3
 
 #Record the BBB playback, playing in Google browser in xvfb virtual screen, as MP4 video
 ffmpeg -y -nostats -s 1280x800 \
-      # -draw_mouse 0 \
-#	-framerate 30 \
-#	-probesize 16M \
+        -draw_mouse 0 \
+	-framerate 30 \
+	-probesize 16M \
 	-f x11grab -thread_queue_size 1024 \
 	-i :$DISPLAY_NUMBER \
 	-f alsa \
-	#-thread_queue_size 1024 \
-#	-itsoffset 0.57 \
+	-thread_queue_size 1024 \
+	-itsoffset 0.57 \
 	-i pulse -ac 2 \
 	-c:v libx264 -c:a aac  \
 	-crf 22  \
